33088e5878911a3cdad712b1e266c74d7666f45c
dtaniwaki <daisuketaniwaki@gmail.com>
2015-12-16 20:48:27 +0900

    Add pusher test

--- test/controllers/PusherActorControllerSpec.scala
+++ test/controllers/PusherActorControllerSpec.scala
@@ -3,20 +3,99 @@ package controllers
 import akka.actor.ActorSystem
 import com.typesafe.config.{ Config, ConfigFactory }
 import play.api.Configuration
+import play.api.libs.json._
 import play.api.mvc._
 import play.api.test._
 import scala.concurrent.Future
 
 object PusherActorControllerSpec extends PlaySpecification with Results {
   val config = Configuration(ConfigFactory.load())
+  implicit val system = ActorSystem("test")
+  val controller = new PusherActorController(config)
 
   "PusherActorController#index" should {
     "should be valid" in {
-      implicit val system = ActorSystem("test")
-      val controller = new PusherActorController(config)
       val result = controller.index().apply(FakeRequest())
       val bodyText = contentAsString(result)
       bodyText must contain("pusher")
     }
+  }
+  if(config.getString("pusher.appId").isDefined) {
+    "#trigger" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.triggerAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherActorController.triggerAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "a_channel",
+            "event" -> "a_event",
+            "body" -> Json.obj(
+              "foo" -> "bar"
+            ).toString,
+            "socket_id" -> "123.345"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj()
+      }
+    }
+    "#channel" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.channelAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherActorController.channelAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "presence-channel"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj(
+          "occupied" -> false,
+          "user_count" -> 0
+        )
+      }
+    }
+    "#channels" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.channelsAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherActorController.channelsAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "prefix" -> "presence-prefix"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj(
+          "channels" -> Json.obj()
+        )
+      }
+    }
+    "#users" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.usersAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherActorController.usersAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "presence-channel"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.arr()
+      }
+    }
+  } else {
+    skipped("pusher.appId is not defined")
   }
 }
--- test/controllers/PusherControllerSpec.scala
+++ test/controllers/PusherControllerSpec.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api.Configuration
+import play.api.libs.json._
 import play.api.mvc._
 import play.api.test._
 import scala.concurrent.Future
@@ -8,13 +9,93 @@ import com.typesafe.config.{ Config, ConfigFactory }
 
 object PusherControllerSpec extends PlaySpecification with Results {
   val config = Configuration(ConfigFactory.load())
+  val controller = new PusherController(config)
 
-  "PusherController#index" should {
+  "#index" should {
     "should be valid" in {
-      val controller = new PusherController(config)
       val result: Future[Result] = controller.index().apply(FakeRequest())
       val bodyText: String = contentAsString(result)
       bodyText must contain("pusher")
     }
+  }
+  if(config.getString("pusher.appId").isDefined) {
+    "#trigger" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.triggerAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherController.triggerAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "a_channel",
+            "event" -> "a_event",
+            "body" -> Json.obj(
+              "foo" -> "bar"
+            ).toString,
+            "socket_id" -> "123.345"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj(
+          "data" -> "{}"
+        )
+      }
+    }
+    "#channel" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.channelAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherController.channelAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "presence-channel"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj(
+          "occupied" -> false,
+          "user_count" -> 0
+        )
+      }
+    }
+    "#channels" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.channelsAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherController.channelsAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "prefix" -> "presence-prefix"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.obj(
+          "channels" -> Json.obj()
+        )
+      }
+    }
+    "#users" should {
+      "should be valid" in {
+        val result: Future[Result] = controller.usersAction().apply(FakeRequest(
+          Helpers.POST,
+          routes.PusherController.usersAction().url,
+          FakeHeaders(Seq(
+            "Content-type" -> "application/json"
+          )),
+          Json.obj(
+            "channel" -> "presence-channel"
+          )
+        ))
+        val bodyText: String = contentAsString(result)
+        Json.parse(bodyText) must be equalTo Json.arr()
+      }
+    }
+  } else {
+    skipped("pusher.appId is not defined")
   }
 }
